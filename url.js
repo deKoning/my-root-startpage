4e16a1640468416c23acf8ba151036c01a466614
Vincent de Koning <deKoning@users.noreply.github.com>
2022-02-21 17:03:57 +0100

    Update url.js
    Update the nameUser from this JS file

--- url.js
+++ url.js
@@ -1,14 +1,16 @@
         // Script to get the parameter from the url
         const queryString = window.location.search;
         console.log(queryString);
-        // Voorbeeld: https://www.mysite.nl/index.html?name=Vincent
+        // Example: https://www.mysite.nl/index.html?name=Vincent
         // value: ?name=Vincent
-        </script>
 
         
-        <script>
+ 
         // Script to show the name in an alert box
           const urlParams = new URLSearchParams(queryString);
 
           const nameUser = urlParams.get('name')
           alert(nameUser);
+        
+        // update the nameUser in the HTML file
+        ocument.getElementById("name").innerHTML = nameUser;
